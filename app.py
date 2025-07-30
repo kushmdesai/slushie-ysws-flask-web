70c6b5f4a89442e16686c19172a0a7e1443edf7b
kush <kushmdesai@icloud.com>
2025-07-30 15:28:54 -0400

    it has been an hour... I have gotten quite a bit of work done. I setup the gemini api calls and now I am trying to make it accept user inputs.

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import os
 
+with open("./sys-config-etc/system_configuration.txt") as f:
+    sys_config = f.read()
+
+load_dotenv()
+API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client()
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     user_name = 'kcoder'
-    return render_template('index.html', user_name=user_name)
+    return render_template('index.html')
 
-@app.route('/testing')
-def testing():
-    user_name = "TESTING"
-    return render_template('testing.html', user_name=user_name)
+@app.route('/chat')
+def chat():
+    user_input = request.form['user_input']
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=[user_input],
+        config=types.GenerateContentConfig(
+            system_instruction=sys_config
+        )
+    )
+@app.route('/about')
+def about():
+    user_name = "about"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(port=5000, debug = True)
-
